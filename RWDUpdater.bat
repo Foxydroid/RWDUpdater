9833253fedf36b74c808ad07304993cb6f85667f
Artem Yakim <80894944+Foxydroid@users.noreply.github.com>
2025-11-02 23:31:44 +0200

    Create RWDUpdater.bat to run PowerShell script

---
+++ RWDUpdater.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set "url=https://raw.githubusercontent.com/Foxydroid/RWDUpdater/refs/heads/main/RWDUpdater.ps1"
+set "tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;"
+
+"%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe" ^
+-Command "%tls% iwr -useb %url% %params% | iex"
+
+pause
+exit /b
