365a38e8e45620d14232b82ed2772dfad76c80ba
Artem Yakim <80894944+Foxydroid@users.noreply.github.com>
2025-11-19 22:55:22 +0200

    I'm removing the pause, since the PowerShell script already has one.

--- RWDUpdater.bat
+++ RWDUpdater.bat
@@ -6,5 +6,4 @@ set "tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]
 "%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe" ^
 -Command "%tls% iwr -useb %url% %params% | iex"
 
-pause
 exit /b
